bc59a122b66cea8c0a99cd1310348539f61d418c
Caleb Foss <calebfoss@gmail.com>
2022-03-04 17:50:44 -0600

    Add example demonstrating p5.VideoRecorder with canvas and audio

--- sketch.js
+++ sketch.js
@@ -1,7 +1,31 @@
+let videoRecorder;
+let cvs;
+let music;
+
 function setup() {
-  // put setup code here
+  cvs = createCanvas(400, 400);
+  music = createAudio("./assets/beat.mp3");
+  noLoop();
 }
 
 function draw() {
-  // put drawing code here
+  background(frameCount, 100, 100);
+  if (frameCount === 360) {
+    videoRecorder.stop();
+  }
+}
+
+function mousePressed() {
+  videoRecorder = new p5.VideoRecorder([cvs, music]);
+  videoRecorder.onFileReady = showAndSaveVideo;
+  videoRecorder.start();
+  music.play();
+  loop();
+}
+
+function showAndSaveVideo() {
+  const videoURL = videoRecorder.url;
+  const vid = createVideo(videoURL);
+  vid.showControls();
+  videoRecorder.save("myVideo");
 }
